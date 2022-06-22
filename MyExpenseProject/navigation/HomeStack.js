f5cd1f1a24482fb07d57d10210c4834e7861b7bb
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-22 16:24:02 +0530

    Home Stack created

---
+++ MyExpenseProject/navigation/HomeStack.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HomeSreen from '../modules/home module/HomeScreen';
+import AddExpense from '../modules/home module/AddExpense';
+
+import {createStackNavigator, Header} from '@react-navigation/stack';
+const Stack = createStackNavigator(); 
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator
+          initialRouteName='HomeScreen'  
+    >
+            <Stack.Screen
+            options={{
+              headerShown:false,
+            }}
+            name="HomeScreen" component={HomeSreen} />
+            <Stack.Screen name="AddExpense" component={AddExpense} />
+    </Stack.Navigator>
+  )
+}
+
+export default HomeStack;
+
