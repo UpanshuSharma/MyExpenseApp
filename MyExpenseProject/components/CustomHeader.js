6698ad8caf80e300120126f2b88a1fac40146cb8
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-23 15:24:46 +0530

    Custom Header & Button formatted

--- MyExpenseProject/components/CustomHeader.js
+++ MyExpenseProject/components/CustomHeader.js
@@ -1,78 +1,73 @@
-import { View, Text, StyleSheet,Image} from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native';
+import React from 'react';
 import {Avatar} from 'react-native-paper';
-
 
 // import MenuComponent from './MenuComponent';
 
 const CustomHeader = ({title}) => {
-
   return (
-
     <View style={styles.container}>
-
-           <View style={styles.main_container}>
-               <Image   style={styles.logo} resizeMode='stretch' source={require('../assets/MyExpense1.png')}/>
-               <Text style={styles.text}>MyExpense</Text>
-           </View>
-           <View style={styles.profile_container}>
-                            <Avatar.Text size={34} label="UN" />
-                            <Text style={styles.username}>UserName</Text>
-                                   
-           </View>
-          
+      <View style={styles.main_container}>
+        <Image
+          style={styles.logo}
+          resizeMode="stretch"
+          source={require('../assets/MyExpense1.png')}
+        />
+        <Text style={styles.text}>MyExpense</Text>
+      </View>
+      <View style={styles.profile_container}>
+        <Avatar.Text size={34} label="UN" />
+        <Text style={styles.username}>UserName</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default CustomHeader;
 
 const styles = StyleSheet.create({
-  
-     container:{
-      // height :55,
-       height:'auto',
-       backgroundColor:'yellow',
-       marginBottom:15,
-       flexDirection:'row',
-      //  padding:10,
-     },
-     main_container:{
-            flex:3,
-            // backgroundColor:'pink',
-            flexDirection:'row',
-            justifyContent:'flex-start',
-            alignItems:'center',
-            padding:5,
+  container: {
+    // height :55,
+    height: 'auto',
+    backgroundColor: 'yellow',
+    marginBottom: 15,
+    flexDirection: 'row',
+    //  padding:10,
+  },
+  main_container: {
+    flex: 3,
+    // backgroundColor:'pink',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 5,
+  },
 
-     },
-    
-     logo:{
-         height:50,
-         width:60,
-        //  marginTop:5,
-        //  marginBottom:10,
-        //  padding:10,
-        //  borderRadius:20,
-     },
-     text:{
-           fontSize:20, 
-           fontWeight:'bold',
-           marginLeft:5,
-     },
+  logo: {
+    height: 50,
+    width: 60,
+    //  marginTop:5,
+    //  marginBottom:10,
+    //  padding:10,
+    //  borderRadius:20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
 
-   profile_container:{
-           flex:3,
-           justifyContent:'flex-end',
-           flexDirection:'row',
-           alignItems:'center',
-          //  backgroundColor:'yellow'
-          marginRight:5,
-      },
-      username:{
-             fontSize:18,
-            //  fontWeight:200,   generating error   ???
-             marginLeft:5,
-      },
-
-})
+  profile_container: {
+    flex: 3,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'center',
+    //  backgroundColor:'yellow'
+    marginRight: 5,
+  },
+  username: {
+    fontSize: 18,
+    //  fontWeight:200,   generating error   ???
+    marginLeft: 5,
+  },
+});
--- MyExpenseProject/components/FormButton.js
+++ MyExpenseProject/components/FormButton.js
@@ -1,34 +1,33 @@
-import {Text,TouchableOpacity , StyleSheet, Dimensions} from 'react-native'
-import React from 'react'
+import {Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import React from 'react';
 
-import { windowHeight } from '../utils/Dimensions'
+import {windowHeight} from '../utils/Dimensions';
 
-const FormButton = ({title , ...rest}) => {
+const FormButton = ({title, ...rest}) => {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-         <Text style={styles.text}>{title}</Text>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-
-const styles=StyleSheet.create({
-      container:{
-          marginTop:10,
-          width:'100%',
-          height:windowHeight/15,
-          backgroundColor:'#2e64e5',
-          padding:10,
-          alignItems:'center',
-          justifyContent:'center',
-          borderRadius:3,
-      },
-      text:{
-          fontSize:18,
-          fontWeight:'bold',
-          color:'#ffffff',
-          fontFamily:'Lato-Regular',
-      }
-})
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    backgroundColor: '#2e64e5',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontFamily: 'Lato-Regular',
+  },
+});
 
 export default FormButton;
