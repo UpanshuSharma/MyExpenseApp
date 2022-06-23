1f86a5b2eb7bf69eaeb5fc09e2e5c9756d015784
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-23 15:22:46 +0530

    EventExpense Screen Added in Home Stack

--- MyExpenseProject/navigation/HomeStack.js
+++ MyExpenseProject/navigation/HomeStack.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import HomeSreen from '../modules/home module/HomeScreen';
 import AddExpense from '../modules/home module/AddExpense';
+import EventExpense from '../modules/home module/EventExpense';
 
 import {createStackNavigator, Header} from '@react-navigation/stack';
-const Stack = createStackNavigator(); 
+const Stack = createStackNavigator();
 
 const HomeStack = () => {
   return (
-    <Stack.Navigator
-          initialRouteName='HomeScreen'  
-    >
-            <Stack.Screen
-            options={{
-              headerShown:false,
-            }}
-            name="HomeScreen" component={HomeSreen} />
-            <Stack.Screen name="AddExpense" component={AddExpense} />
+    <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="HomeScreen"
+        component={HomeSreen}
+      />
+      <Stack.Screen name="AddExpense" component={AddExpense} />
+      <Stack.Screen name="EventExpense" component={EventExpense} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default HomeStack;
-
