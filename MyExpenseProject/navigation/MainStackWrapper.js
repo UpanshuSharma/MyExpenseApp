cdec11b66ee1e1d7aa8b4e071b93131ef18600f1
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-23 15:25:43 +0530

    fix route & props

--- MyExpenseProject/navigation/MainStackWrapper.js
+++ MyExpenseProject/navigation/MainStackWrapper.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import AppStack from './AppStack';
+
+
 const Stack = createStackNavigator();
 import CustomHeader from '../components/CustomHeader';
-import Routes from './Routes';
 
 const MainStackWrapper = () => {
   return (
       <Stack.Navigator
-       
            screenOptions={{
-                        header: props =><CustomHeader />,
-                        
+                        header: props =><CustomHeader />,           
            }}
-      
       >
                 <Stack.Screen name="Main" component={AppStack} />
       </Stack.Navigator>
