8fa9b5f646022ee2ace63df44745c473587ad200
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-28 15:20:40 +0530

    Scroll View added in signUp Screen

--- MyExpenseProject/screens/SignUpScreen.js
+++ MyExpenseProject/screens/SignUpScreen.js
@@ -7,115 +7,122 @@ import {
   Platform,
   StyleSheet,
   KeyboardAvoidingView,
-
+  ScrollView,
 } from 'react-native';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import SocialButton from '../components/SocialButton';
-import { AuthContext } from '../navigation/AuthenticationProvider';
-
-
+import {AuthContext} from '../navigation/AuthenticationProvider';
 
 const SignUpScreen = ({navigation}) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
 
-  const {register,googleLogin} = useContext(AuthContext)
 
+
+  
 
   return (
-    
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.keyboardView}
-    >
+    <ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardView}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Create an account</Text>
 
-    <View style={styles.container}>
-      <Text style={styles.text}>Create an account</Text>
+         {/** <FormInput
+            labelValue={username}
+            onChangeText={userName => setUsername(userName)}
+            placeholderText="User Name"
+            iconType="user"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+          /> */}
 
-      <FormInput
-        labelValue={email}
-        onChangeText={(userEmail) => setEmail(userEmail)}
-        placeholderText="Email"
-        iconType="user"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+          <FormInput
+            labelValue={email}
+            onChangeText={userEmail => setEmail(userEmail)}
+            placeholderText="Email"
+            iconType="mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
 
-      <FormInput
-        labelValue={password}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-        placeholderText="Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
+          <FormInput
+            labelValue={password}
+            onChangeText={userPassword => setPassword(userPassword)}
+            placeholderText="Password"
+            iconType="lock"
+            secureTextEntry={true}
+          />
 
-      <FormInput
-        labelValue={confirmPassword}
-        onChangeText={(userPassword) => setConfirmPassword(userPassword)}
-        placeholderText="Confirm Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
+          <FormInput
+            labelValue={confirmPassword}
+            onChangeText={userPassword => setConfirmPassword(userPassword)}
+            placeholderText="Confirm Password"
+            iconType="lock"
+            secureTextEntry={true}
+          />
 
-      <FormButton
-        title="Sign Up"
-        onPress={() => register(email, password)}
-      />
+          <FormButton
+            title="Sign Up"
+            onPress={() => register(email, password)}
+          />
 
-      <View style={styles.textPrivate}>
-        <Text style={styles.color_textPrivate}>
-          By registering, you confirm that you accept our{' '}
-        </Text>
-        <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
-          <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
-            Terms of service
-          </Text>
-        </TouchableOpacity>
-        <Text style={styles.color_textPrivate}> and </Text>
-        <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
-          Privacy Policy
-        </Text>
-      </View>
+          <View style={styles.textPrivate}>
+            <Text style={styles.color_textPrivate}>
+              By registering, you confirm that you accept our{' '}
+            </Text>
+            <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
+              <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
+                Terms of service
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.color_textPrivate}> and </Text>
+            <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
+              Privacy Policy
+            </Text>
+          </View>
 
-      {Platform.OS === 'android' ? (
-        <View>
-        <SocialButton
-        title="Sign Up with Facebook"
-        btnType="facebook"
-        btnColor="#4867aa"
-        backgroundColor="#e6eaf4"
-        // onPress={() => fbLogin()}
-      />
+          {Platform.OS === 'android' ? (
+            <View>
+              <SocialButton
+                title="Sign Up with Facebook"
+                btnType="facebook"
+                btnColor="#4867aa"
+                backgroundColor="#e6eaf4"
+                // onPress={() => fbLogin()}
+              />
 
-      <SocialButton
-        title="Sign Up with Google"
-        btnType="google"
-        btnColor="#de4d41"
-        backgroundColor="#f5e7ea"
-       onPress={() => googleLogin()}
-      />
+              <SocialButton
+                title="Sign Up with Google"
+                btnType="google"
+                btnColor="#de4d41"
+                backgroundColor="#f5e7ea"
+                onPress={() => googleLogin()}
+              />
+            </View>
+          ) : null}
+
+          <TouchableOpacity
+            style={styles.navButton}
+            onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.navButtonText}>Have an account? Sign In</Text>
+          </TouchableOpacity>
         </View>
-      ) : null}
-
-      <TouchableOpacity
-        style={styles.navButton}
-        onPress={() => navigation.navigate('Login')}>
-        <Text style={styles.navButtonText}>Have an account? Sign In</Text>
-      </TouchableOpacity>
-    </View>
-  </KeyboardAvoidingView>
- 
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
 export default SignUpScreen;
 
 const styles = StyleSheet.create({
-  keyboardView:{
-    flex:1,
+  keyboardView: {
+    flex: 1,
   },
   container: {
     backgroundColor: '#f9fafd',
