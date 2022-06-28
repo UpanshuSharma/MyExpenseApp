3d283ec791cae91a2a7866fdb9bcf9c288c41882
Upanshu Sharma <upanshupathak@gmail.com>
2022-06-28 15:17:04 +0530

    logout button added

--- MyExpenseProject/modules/my account module/MyAccountScreen.js
+++ MyExpenseProject/modules/my account module/MyAccountScreen.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,Button } from 'react-native'
+import React ,{useContext}from 'react'
+
+import { AuthContext } from '../../navigation/AuthenticationProvider'
+
+
 
 const MyAccountScreen = () => {
+   const {logout}=useContext(AuthContext);
   return (
     <View>
       <Text>MyAccountScreen</Text>
+      
+      <Button
+      title='Logout'
+       onPress={()=>logout()}
+      />
     </View>
   )
 }
